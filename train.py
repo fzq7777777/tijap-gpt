32bf2fb1e395dff00bfe910ba3f15eb18b011e0a
Bacchus <37797864+fzq7777777@users.noreply.github.com>
2025-04-10 11:08:49 +0800

    first commit

---
+++ train.py
@@ -0,0 +1,443 @@
+import os
+import math
+import time
+import inspect
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import tiktoken
+import numpy as np
+from torch.distributed import init_process_group, destroy_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+# -----------------------------------------------------------------------------
+
+class CausalSelfAttention(nn.Module):
+    #这些头就像是并行的流，他们的输出被拼接起来
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # 768,3*768,参考gpt2
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1      # 初始化缩放比例为1
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+    def forward(self, x):
+        B, T, C = x.size()     # 批量大小、序列长度、嵌入维度（n_embd）
+        # 在 GPT-2（124M）模型中，n_head=12，hs=64，因此 nh*hs=768 个通道在 Transformer 架构中。
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # flash attention 已经出了3篇论文了
+        # Fused Kernels 减少内核调用开销‌,提高内存访问效率‌,优化指令流水线
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True) 
+        y = y.transpose(1, 2).contiguous().view(B, T, C) 
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu    = nn.GELU(approximate='tanh')
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+    #层归一化-》自注意力-》层归一化-》前馈网络-》残差流
+    #注意力是归约操作，MLP是映射操作
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),               #token embeddings
+            wpe = nn.Embedding(config.block_size, config.n_embd),               #position embeddings
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),  #12 transformer blocks
+            ln_f = nn.LayerNorm(config.n_embd),                                 #final layer 线性归一化层
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)  #语音模型头
+
+        # 权重共享
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # 初始化参数，遍历
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02  # 0.02是gpt2的初始化标准差,1/sqrt(768)=0.0365,1/sqrt(1600)=0.025
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5        
+                #gpt2论文中提到的残差层权重按 1/sqrt(n) 的系数缩放，2是因为Block中有注意力和MLP
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # forward the token and posisition embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
+
+    
+
+    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+        # 所有需要求导的参数
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # 二维以上的参数会进行权重衰减处理
+        # 矩阵乘法和嵌入操作中的所有权重张量都会衰减，而所有偏置项和层归一化项则不会。
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        if master_process:
+            print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+            print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # 创建 AdamW 优化器，并在可用的情况下使用融合版本
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        if master_process:
+            print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
+
+# -----------------------------------------------------------------------------
+
+def load_tokens(filename):
+    npt = np.load(filename)
+    npt = npt.astype(np.int32) # added after video
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
+class DataLoaderLite:
+    def __init__(self, B, T, process_rank, num_processes, split):
+        self.B = B
+        self.T = T
+        self.process_rank = process_rank
+        self.num_processes = num_processes
+        assert split in {'train', 'val'}
+
+        # get the shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+        self.reset()
+
+    def reset(self):
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # 在张量中提升位置
+        self.current_position += B * T * self.num_processes
+        # 如果加载下一批数据会超出范围，就转到下一个分片继续处理。
+        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            self.current_position = B * T * self.process_rank
+        return x, y
+
+# -----------------------------------------------------------------------------
+# takes tokens, mask, and logits, returns the index of the completion with the lowest loss
+
+def get_most_likely_row(tokens, mask, logits):
+    
+    shift_logits = (logits[..., :-1, :]).contiguous()
+    shift_tokens = (tokens[..., 1:]).contiguous()
+    flat_shift_logits = shift_logits.view(-1, shift_logits.size(-1))
+    flat_shift_tokens = shift_tokens.view(-1)
+    shift_losses = F.cross_entropy(flat_shift_logits, flat_shift_tokens, reduction='none')
+    shift_losses = shift_losses.view(tokens.size(0), -1)
+    
+    shift_mask = (mask[..., 1:]).contiguous() 
+    masked_shift_losses = shift_losses * shift_mask
+    
+    sum_loss = masked_shift_losses.sum(dim=1)
+    avg_loss = sum_loss / shift_mask.sum(dim=1)
+    
+    pred_norm = avg_loss.argmin().item()
+    return pred_norm
+
+# -----------------------------------------------------------------------------
+# 训练
+
+# torchrun 命令会设置环境变量 RANK、LOCAL_RANK 和 WORLD_SIZE
+ddp = int(os.environ.get('RANK', -1)) != -1     # 是否DDP运行
+if ddp:
+    # 使用 DDP 需要 CUDA，根据 rank 来适当地设置设备
+    assert torch.cuda.is_available(), "for now i think we need CUDA for DDP"
+    init_process_group(backend='nccl')
+    ddp_rank = int(os.environ['RANK'])
+    ddp_local_rank = int(os.environ['LOCAL_RANK'])      # 多节点会用到
+    ddp_world_size = int(os.environ['WORLD_SIZE'])
+    device = f'cuda:{ddp_local_rank}'
+    torch.cuda.set_device(device)
+    master_process = ddp_rank == 0  # 此 master_process 将执行日志记录、检查点处理等操作。
+    device_type = "cuda" if device.startswith("cuda") else "cpu"
+else:
+    # 用不了ddp的情况。。。心塞
+    ddp_rank = 0
+    ddp_local_rank = 0
+    ddp_world_size = 1
+    master_process = True
+    # 尝试自动检测设备
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        device = "mps"
+    print(f"using device: {device}")
+    device_type = device
+    
+
+# 方便复现
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
+enc = tiktoken.get_encoding("gpt2")
+
+total_batch_size = 524288 # 2**19, ~0.5M, 模拟GPT-3 Small
+B = 4      # 如果遇到内存不足，可以尝试减小批次大小
+T = 1024    # 序列长度
+# 计算梯度累积步数
+assert total_batch_size % (B * T * ddp_world_size) == 0, "make sure total_batch_size is divisible by B * T * ddp_world_size"
+grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
+# 只在 master_process 打印一次
+if master_process:
+    print(f"total desired batch size: {total_batch_size}")
+    print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
+
+train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="train")
+val_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="val")
+      
+# 启用TF32精度，一个符号位，八个指数位,精度最后13位被截断丢弃，最终只有19位，为了提升速度，降低了精度。指数决定范围，精度决定精确程度
+torch.set_float32_matmul_precision('high')
+
+# create model
+model = GPT(GPTConfig(vocab_size=50304))
+# model = GPT.from_pretrained("gpt2") # or init from OpenAI GPT-2
+model.to(device)
+use_compile = False # torch.compile interferes with HellaSwag eval and Generation. TODO fix
+if use_compile:
+    # torch.compile将整个神经网络编译成一个不涉及python interpreter的单一对象；减少了Python的开销和GPU与HBM之间的读写次数。
+    # 内存遍布整个芯片，如 L1和L2 cache，但这些内存与HBM中内存的存储方式不同，再具体我就不懂了。
+    # 大部分都存储在HBM中，所以读写次数越多，用时就越长
+    model = torch.compile(model)
+if ddp:
+    # 一旦反向传播结束，DDP会对所有等级上的梯度进行平均，每个都会得到这个平均值
+    model = DDP(model, device_ids=[ddp_local_rank])
+raw_model = model.module if ddp else model # always contains the "raw" unwrapped model
+
+max_lr = 6e-4               # 最大学习率
+min_lr = max_lr * 0.1       # 最小学习率
+warmup_steps = 715  #gpt3论文提到warmup over the first 3.75 million tokens,所以3750000000 / 2**19 = 715
+max_steps = 19073 # 10e9 / 2**19
+def get_lr(it):
+    # 1) 预热
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    # 2) 如果大于 max_steps, 返回最小学习率
+    if it > max_steps:
+        return min_lr
+    # 3) 两者之间，使用余弦衰减
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # 系数从 1 开始，依次递减至 0
+    return min_lr + coeff * (max_lr - min_lr)
+
+# 优化器
+optimizer = raw_model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device_type=device_type)
+
+# 创建日志目录，在此目录下写入检查点并进行记录
+log_dir = "log"
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, f"log.txt")
+with open(log_file, "w") as f:      # 打开以写入模式清空文件
+    pass
+
+for step in range(max_steps):
+    t0 = time.time()
+    last_step = (step == max_steps - 1)
+
+    # 评估
+    if step % 250 == 0 or last_step:
+        model.eval()
+        val_loader.reset()
+        with torch.no_grad():
+            val_loss_accum = 0.0
+            val_loss_steps = 20
+            for _ in range(val_loss_steps):
+                x, y = val_loader.next_batch()
+                x, y = x.to(device), y.to(device)
+                # 混合精度尝试将每个操作与最适合它的数据类型相匹配，这能够减少运行时间和内存占用量。torch.autocast 实例充当上下文管理器的角色
+                with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+                    logits, loss = model(x, y)
+                loss = loss / val_loss_steps        # cross_entropy的参数reduction='mean'默认是对每个batch的loss求平均值
+                val_loss_accum += loss.detach()     # 从当前计算图中分离，不计算梯度
+        if ddp:
+            dist.all_reduce(val_loss_accum, op=dist.ReduceOp.AVG)
+        if master_process:
+            print(f"validation loss: {val_loss_accum.item():.4f}")
+            with open(log_file, "a") as f:
+                f.write(f"{step} val {val_loss_accum.item():.4f}\n")
+            if step > 0 and (step % 5000 == 0 or last_step):
+                # optionally write model checkpoints
+                checkpoint_path = os.path.join(log_dir, f"model_{step:05d}.pt")
+                checkpoint = {
+                    'model': raw_model.state_dict(),
+                    'config': raw_model.config,
+                    'step': step,
+                    'val_loss': val_loss_accum.item()
+                }
+                
+                torch.save(checkpoint, checkpoint_path)
+
+
+    # 定期生成一下看看
+    if ((step > 0 and step % 250 == 0) or last_step) and (not use_compile):
+        model.eval()
+        num_return_sequences = 4
+        max_length = 32
+        tokens = enc.encode("Hello, I'm a language model,")
+        tokens = torch.tensor(tokens, dtype=torch.long)
+        tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+        xgen = tokens.to(device)
+        sample_rng = torch.Generator(device=device)
+        sample_rng.manual_seed(42 + ddp_rank)
+        while xgen.size(1) < max_length:
+            # forward the model to get the logits
+            with torch.no_grad():
+                with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+                    logits, loss = model(xgen) # (B, T, vocab_size)
+                # take the logits at the last position
+                logits = logits[:, -1, :] # (B, vocab_size)
+                # get the probabilities
+                probs = F.softmax(logits, dim=-1)
+                # 对 50 个样本进行 top-k 抽样(huggingface pipeline default)
+                # topk_probs (5, 50), topk_indices (5, 50)
+                topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+                # 从topk_probs中采样1个
+                ix = torch.multinomial(topk_probs, 1, generator=sample_rng) # (B, 1)
+                # 获取索引
+                xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+                # 拼接
+                xgen = torch.cat((xgen, xcol), dim=1)
+        # print the generated text
+        for i in range(num_return_sequences):
+            tokens = xgen[i, :max_length].tolist()
+            decoded = enc.decode(tokens)
+            print(f"rank {ddp_rank} sample {i}: {decoded}")
+
+    
+    model.train()
+    optimizer.zero_grad()
+    loss_accum = 0.0
+    for micro_step in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        # 反向梯度同步只在 micro_step 的最后一步进行
+        if ddp:
+            model.require_backward_grad_sync = (micro_step == grad_accum_steps - 1)
+        with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+        # 必须对损失值进行调整以考虑梯度累积，因为在每次的反向传播中，梯度只是累加起来
+        loss = loss / grad_accum_steps
+        loss_accum += loss.detach()
+        loss.backward()
+    # 在DDP容器之外，没有被平均    
+    if ddp:
+        dist.all_reduce(loss_accum, op=dist.ReduceOp.AVG)
+    # clip the global norm of the gradient at 1.0,防止模型在梯度大小方面受到过大的冲击    
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)      # clip the global norm of the gradient at 1.0,防止模型在梯度大小方面受到过大的冲击
+    # 确定并设定本次迭代的学习率
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    optimizer.step()
+    if device_type == "cuda":
+        torch.cuda.synchronize() # 等待 GPU 完成工作
+    t1 = time.time()
+    dt = t1 - t0 # time difference in seconds
+    tokens_processed = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
+    tokens_per_sec = tokens_processed / dt          # 每秒处理的 token 数
+    if master_process:
+        print(f"step {step:5d} | loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
+        with open(log_file, "a") as f:
+            f.write(f"{step} train {loss_accum.item():.6f}\n")
+
+if ddp:
+    destroy_process_group()
